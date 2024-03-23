2f9b283e41db96712e04e65f0a4cb303ccba8524
BlissMediaSolutions <danielle@bliss.net.au>
2024-03-23 17:25:46 +1100

    Changed functionality - all directives on single line

--- user_upload.php
+++ user_upload.php
@@ -1,80 +1,120 @@
 <?php
 
-    $host = "127.0.0.1";        //default IP of MariaDB
-    $username = "username";     //default dummy username for MariaDB   
-    $password = "password";     //default dummy password for MariaDB
-    $database = "database";     //default dummy name for database
-    $filename = "somefile.csv"  //default dummy name for CSV file
-    
-    if (sizeof($argv) <= 1) {
-        echo "Error in Command.  Missing Directive instruction\n";
+require 'rb-mysql.php';
+
+$host = $username = $password = $database = $filename = $errorMsg = $dryrun = "";
+$dir = "";
+
+if ($argc <= 1) {
+    echo "Error in Command.  Missing Directive instruction\n";
+    exit (helpInstructions());
+} else {
+    $dir = "--help";
+    if (checkDirectiveExists($dir, $argv, $argc) == TRUE) { //If directives includes Help - then just display help & exit.
         exit (helpInstructions());
     } else {
-        menu($argv[1]);
-        while (true) {
-    
-            $line = readline("Directive: ");
-    
-            if (($line === false) OR ($line === null) OR (empty($line))) {
-                break;
+        if (!checkBaseDirectives()) {    //If BaseDirectives failed, then we'll display Help as a courtesy & exit.
+            echo "\n";
+            exit(helpInstructions());
+        } else {
+            if ($dryrun) {
+                //perform a dry run
             }
-            menu($line);
+            if (!createDBConnection()) {  //IF Database connection failed, then we'll display Help as a courtesy & exit.
+                exit(helpInstructions());
+            } else if (!$dryrun) {          //If its a dry run, then we're not using\testing the database
+                //do something
+                R::close();
+            }
         }
     }
-    
-    function menu($direct) {
-        switch ($direct) {
-            case "--help":
-                helpInstructions();
-                break;
-            case str_starts_with($direct, "--file"):  //assumes file name (or location) wont contain spaces
-                $file = explode(" ", $direct)
-                echo count($file) != 2 ? "missing or invalid filename\n" : processCSV($file[1]);
-                break;
-            case "--create_table":
-                //todo create database table
-                echo "database table created\n";
-                break;
-            case "--dry_run":
-                //todo perform dry run
-                echo "dry run\n";
-                break;
-            case str_starts_with($direct, "-u"): //username may contain spaces - so will need to rejoin elements
-                $user = explode(" ", $direct);
-                echo count($user)!= 2 ? "missing for invalid password\n" : count($user)."\n";
-                break;
-            case str_starts_with($direct, "-p"):  //password may contain spaces - so will need to rejoin elements
-                $passw = explode(" ", $direct);
-                echo count($passw) !> 2 ? "missing for invalid password\n" : $direct."\n";
-                break;
-            case str_starts_with($direct, "-h"):  //assumes hostname wont contain spaces
-                $hostadd = explode(" ", $direct);
-                echo count($hostadd) != 2 ? "missing or invalid hostname\n" : "Host name changed to: ".$host = $hostadd[1];
-                break;
-            case str_starts_with($direct, "-d"):  //assumes database name wont contain spaces
-                $datab = explode(" ", $direct);
-                echo count($datab) != 2 ? "missing or invalid database name\n" : "Database name changed to: ".$database = $datab[1]."\n";
-                break;
-            default:
-                echo "imvalid directive command\n";
-                break;
-        }
+}
+
+//Simple function to check if a given directive exists
+function checkDirectiveExists($dir, $argv, $argc) {
+    return (in_array($dir, $argv));
+}
+
+//Check to see if the base required directives to function exist
+//CreateTable direective is optional, because if table exists it will just append data
+function checkBaseDirectives() {
+    $sOpts = "d:h:u:p:";
+    $lOpts = array("file:","create_table::","dry_run::");
+    $direct = getopt($sOpts, $lOpts);
+    global $database, $host, $username, $password, $filename, $errorMsg, $dryrun;
+
+    if (in_array("d", array_keys($direct)))
+        $database = $direct['d'];
+    else
+        $errorMsg .= "Error - Missing directive & name for database\n";
+
+    if (in_array("h", array_keys($direct)))
+        $host = $direct['h'];
+    else
+        $errorMsg .= "Error - Missing directive & name for database host\n";
+
+    if (in_array("u", array_keys($direct)))
+        $username = $direct['u'];
+    else
+        $errorMsg .= "Error - Missing directive & name for database user\n";
+
+    if (in_array("p", array_keys($direct)))
+        $password = $direct['p'];
+    else 
+        $errorMsg .= "Error - Missing directive & password for database password\n";
+
+    if (in_array("file", array_keys($direct)))
+        $filename = $direct['file'];
+    else
+        $errorMsg .= "Error - Missing directive & filename for CSV file";
+
+    if (in_array("dry_run", array_keys($direct)))
+        $dryrun = TRUE;
+    else
+        $dryrun = FALSE;
+
+    if ((!empty($errorMsg)) OR ($errorMsg != NULL)) {
+        echo $errorMsg;
+        return FALSE;
+    } else {
+        return TRUE;
     }
+}
 
+//Create the Database Connection using directive values.  We still use a try\catch incase the RedBean file is missing (RedBean doesn't pass PDO errors back)
+function createDBConnection() {  
+    try {
+        R::setup('mysql:host='.$GLOBALS['host'].";dbname=".$GLOBALS['database'].",".$GLOBALS['username'].",".$GLOBALS['password']);
+        
+        //Test the RedBean Connection, if it fails then display error message & help instructions
+        if (!$isConnected = R::testConnection()) {
+            echo "An Error has occured with the database connection.  Please check the database name & host and that the user account has access\n";
+        }
 
+        if ($isConnected == FALSE)
+            return FALSE;
+        else {
+            return TRUE;
+        }
+        //Sometimes its easier to test with PDO - so below line can be used for that.
+        //$db = new PDO('mysql:host='.$GLOBALS['host'].';dbname='.$GLOBALS['database'].','.$GLOBALS['username'].','.$GLOBALS['password']);
+    } catch (Exception $e) {
+        echo "Problem with database connection: ".$e->getmessage();
+        exit(helpInstructions());
+    }
+}
 
-
-    function helpInstructions()
+function helpInstructions()
     {
         $text = "\n     |>> ** USER_UPLOAD DIRECTIVES ** <<|     \n".
-            "   --help = Display these instructions for directive usage\n".
-            "   --file [csv filename] = this is the name of the CSV file to be parsed into the database\n".
-            "   --create_table = will create the users db table\n".
-            "   --dry_run = Peforms a dry run on the csv file, but doesn't actually write any data to the Db\n";
-            "   -u = set the database username\n".
-            "   -p = set the database password\n".
-            "   -h = set the database host\n".
-            "   -d = set the database name\n";
+            "--help = Display these instructions for directive usage\n".
+            "--file [csv filename] = this is the name of the CSV file to be parsed into the database\n".
+            "--create_table = will create the users db table\n".
+            "--dry_run = Peforms a dry run on the csv file, but doesn't actually write any data to the Db\n".
+            "-u [username] = set the database username\n".
+            "-p [password] = set the database password\n".
+            "-h [host] = set the database host\n".
+            "-d [database] = set the database name\n";
 
         echo $text."\n";
     }
