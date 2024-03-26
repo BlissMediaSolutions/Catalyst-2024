07b35495083f0e3cee972dd0ecd04116ff395138
BlissMediaSolutions <danielle@bliss.net.au>
2024-03-26 13:55:26 +1100

    All should be complete & working

--- README.md
+++ README.md
@@ -3,30 +3,60 @@
 #### Author:
 * Danielle Walker
 
-#### Designed / Tested on:
+#### Designed for / Tested on:
 * Debian 11 (Bullseye)   
 * PHP 8.1.27
 * MariaDB 10.5.23
 
 #### Assumptions:
+* The order in which directives are entered should not matter.
 * If the Help directive was included (with all other directives) we assume the user needs help & just display the Help without trying to access the database or process any data.
 * The directives for: database, host, username, password, filename & create_table should all be 'required'.  If any are excluded it will supply an error message with Help displayed.
-* When performing a 'dry_run' then all required directives are present.  Its assumed a dry-run doesn't need to check the database connection & its just concerned with parsing data.
+* When performing a 'dry_run' then all required directives are present.  A dry_run will test the connection to the database, but it will not drop or create a table.
+* The CSV file being used is valid - it will check the file exists & has a CSV extension, but it will not check the structure or encoding of the file.
 * For MySQL\MariaDB Host - its assumed the user understands IP addresses & knows what Port its listening on.
 * Its assumed the database (as specifed in directive) will already exist.  This script wont create a database.
 * Its assumed username\password supplied in directives will already have appropriate access\permissions for the database.
-* Database Name wont contain spaces unless its encapsulated within quoations marks 
-* Username wont contain spaces unless its encapsulated within quotation marks
-* Password wont contain spaces unless its encapsulated within quotation marks
+* Database Name wont contain spaces unless its encapsulated within quoations marks. 
+* Username wont contain spaces unless its encapsulated within quotation marks.
+* Password wont contain spaces unless its encapsulated within quotation marks.
 * CSV filename wont contain spaces unless its encapsulated within quotation marks.
 * Performing a `dry_run` on the csv will require all directives - the same as when not performing a dry_run.
-* `create_table` directive is excluded & the table exists, then data will be appended to it.
+* The user has some experience running PHP Scripts in a CLI.
 
 #### Limiations:
-* While the script will check if a file exists (as given in directive) & that the directive filename has a CSV extension - it wont actually check the encoding of the specifed CSV file in the directive.  i.e: It wont validate if its UTF-8 string encoding.
+* While the script will check if a file exists (as given in directive) & that the directive filename has a CSV extension - it wont actually check the structure or encoding of the specifed CSV file in the directive.  i.e: It wont validate if its UTF-8 string encoding.
+* Having a directive repeated may cause issues i.e: `php user_upload.php --dry_run -d catalyst -h 127.0.0.1:3306 -u root -p password --file users.csv --create_table --dry_run`.  While directives are validated to an extent, this does not include them being repeated.
+* This hasn't been tested against EOL versions of PHP ~ like PHP versions 3,4 & 5.
+* This hasn't been tested on a Windows OS or Mac OS.
+* While getOpt is preferred for reading directives passed (due to requirements), it does experience issues when having directives with no value - like create_table & dry_run.  Thus these need to be checked in argv array as well.
 
 #### Usage:
-The file rb-mysql.php needs to be included in the same directory as where the script will run (so it can then use RedBean ORM).
+When using the script, various directives (or arguments) need to be supplied - most of these are required.  These are:
+--file [csv filename] *required     - csv file to be processed
+--create_table *required        
+--dry_run                           - perform a dry run of the CSV file without writing to database
+-d [database name] *required        - the database name
+-h [hostname ] *required            - the host address for the database
+-u [user name] *required            - username for access to the database
+-p [password ] *required            - password for access to the database
+--help                              - display Help instructions on the screen.
+Using\including dry_run directive is optional
+
 To display the Help instructions:
 `php user_upload.php --help`
+To perform a dry_run (this requires all directives):
+`php user_upload.php -d catalyst -h 127.0.0.1:3306 -u root -p password --file users.csv --create_table --dry_run`
+To process the CSV and write to the database:
+`php user_upload.php -d catalyst -h 127.0.0.1:3306 -u root -p password --file users.csv --create_table`
+Whenever the Help directive is included like with this, then only the help instructions get displayed and nothing else will happen.
+`php user_upload.php -d catalyst -h 127.0.0.1:3306 -u root -p password --file users.csv --dry_run --help`
+If (required) directives are excluded, but Help directive is included - then only Help instructions display.
+`php user_upload.php -d catalyst -h 127.0.0.1:3306 --help`
+If required directive(s) is missing, an appropriate error will be displayed for what directives are missing.
 
+#### Future Improvements \ upgrades:
+* Use an ORM for database queries (RedBean not appropriate due to limitations)
+* Check the structure & encoding of the CSV file
+* Could have used a Class for Database queries - but PDO already an object, & having a wrapper for PDO didn't appear to make much sense.
+* Reduce\replace Global variables - possibly change these to Enum.
--- user_upload.php
+++ user_upload.php
@@ -1,44 +1,37 @@
 <?php
 
+error_reporting(-1);
 $host = $username = $password = $database = $filename = $errorMsg = $dryrun = $createT = "";
 $dir = $db = "";
 
 /* Main program start */
 if ($argc <= 1) {
-    echo "Error in Command.  Missing Directive instruction\n";
+    echo "Error in Command.  Missing Directive instruction\n";  //No directives were included
     exit (helpInstructions());
 } else {
-    $dir = "--help";
-    if (checkDirectiveExists($dir, $argv, $argc) == TRUE) { //If directives includes Help - then just display help & exit.
+    if (in_array("--help", $argv)) {                 // If directives include Help - then we just display help & exit.
         exit (helpInstructions());
     } else {
-        if (!checkBaseDirectives($argc)) {    //If BaseDirectives failed, then we'll display Help as a courtesy & exit.
+        if (!checkBaseDirectives($argc, $argv)) {          //If BaseDirectives failed, then we'll display Help as a courtesy & exit.
             echo "\n";
             exit(helpInstructions());
         } else {                        
-            if ($GLOBALS['dryrun']) {           //Everything passed so far, but dryrun is TRUE - so we'll just display the data.
-                readCSV($db);
-                exit();
-            }
-            $db = createDBConnection();
-            createTable($db);
-            readCSV($db);
+            $db = createDBConnection();             //Connect to the database
+            if ($GLOBALS['dryrun'] == FALSE)        //We only perform create table if its not a dry_run
+                createTable($db);
+            readCSV($db);                           //Process the CSV file - for either dryrun or writing to DB.
             $db=null;
-            }
+            echo "\nclosed database connection...\n";
+        }
     }
 }
 /* Main program end */
 
-//Simple function to check if a given directive exists
-function checkDirectiveExists($dir, $argv, $argc) {
-    return (in_array($dir, $argv));
-}
-
 //Check to see if the base required directives to function exist & set boolean flag for optional directives.
 //create_table directive is required as email field is unique but then requires an empty space for invalid emails (so table gets dropped & recreated each time)
-function checkBaseDirectives($argc) {
+function checkBaseDirectives($argc, $argv) {
     $sOpts = "d:h:u:p:";
-    $lOpts = array("file:","create_table::","dry_run::");
+    $lOpts = array("file:","create_table:","dry_run::");
     $direct = getopt($sOpts, $lOpts, $count);
     global $database, $host, $username, $password, $filename, $errorMsg, $dryrun, $createT;
 
@@ -78,19 +71,19 @@ function checkBaseDirectives($argc) {
     else
         $errorMsg .= "Error - Missing directive & filename for CSV file\n";
 
+    //When multiple directives with no value like dry_run - getOpt can have issues, so we check argv arrary as well.
     //dry_run directive is OPTIONAL. Set a flag value as to if its present
-    if (in_array("dry_run", array_keys($direct)))
+    if ((in_array("dry_run", array_keys($direct))) OR (in_array("--dry_run", $argv)))
         $dryrun = TRUE;
     else
         $dryrun = FALSE;
 
-    //check if create_table directive is present - is required when not dry_run
-    if (!$dryrun) {
-        if (in_array("create_table", array_keys($direct)))
-            $createT = TRUE;
-        else
-            $errorMsg .= "Error - Missing directive to create database table\n";
-    }
+    //When multiple directives with no value like create_table - getOpt can have issues, so we check argv arrary as well.
+    //check if create_table directive is present - as its required.
+    if ((in_array("create_table", array_keys($direct))) OR (in_array("--create_table", $argv)))
+        $createT = TRUE;
+    else
+        $errorMsg .= "Error - Missing directive to create database table\n";
 
     //Basic check to see if $filename has .csv extension in name. **This doesn't actually check if its actually a properly structured\encoded CSV file.
     $ext = new SplFileInfo($filename);
@@ -111,63 +104,71 @@ function readCSV($dBase) {
     if (!file_exists($GLOBALS['filename']))
         exit("Error - Unable to locate ".$GLOBALS['filename']." file. Please check the file actually exists\n");
 
-    //We want to exclude the firstline, so add a flag for it.
+    $errString = "";
+    $count = 0;
+    
+    //We assume firstline is header & want to exclude the firstline, so add a flag for it.
     $firstline = TRUE;
 
-    if ($GLOBALS['dryrun']);
-        echo "Performing a dry_run of the CSV data (this is what would be written to the database)...\n";
+    if ($GLOBALS['dryrun'] == TRUE)
+        echo "Performing a dry_run of the CSV data (this is roughly what would be written to the database)...\nNote - Data which breaches database constraint violations will still appear below\n\n";
    
     if (($file = fopen($GLOBALS['filename'], "r")) !== FALSE) {
         while (($csvData = fgetcsv($file, 1000, ',')) !== FALSE) {
             if (!$firstline) {
-                //cleanup & format our CSV data & replace existing array data emails if invalid
+                //cleanup & format our CSV data in the array
                 $csvData[0] = trim(ucwords(strtolower($csvData[0])));
                 $csvData[1] = trim(ucwords(strtolower($csvData[1])));
                 $csvData[2] = trim(strtolower($csvData[2]));
                 
-                //If its not a dryrun & there's an invalid email address - write to error log
-                if ((!$GLOBALS['dryrun']) && (!filter_var($csvData[2], FILTER_VALIDATE_EMAIL)))
-                    //echo "invalid email: ".$csvData[2]."\n";
-                
-                //replace invalid email address in array with blankspace
-                (filter_var(trim(strtolower($csvData[2])), FILTER_VALIDATE_EMAIL)) ? $csvData[2] = trim(strtolower($csvData[2])) : $csvData[2] = "";
-
-                if (!$GLOBALS['dryrun']) {
+                if ($GLOBALS['dryrun'] == FALSE) {
                     try {
-                        //Find & replace apostrophes in strings - just for writing to the database
+                        //Find & escape apostrophes in strings - just for writing to the database cause we don't want this for dryrun
                         $fname = str_replace("'","''",$csvData[0]);  
                         $flastname = str_replace("'","''",$csvData[1]);
                         $femail = str_replace("'","''",$csvData[2]);
-                        //insert into database
-                        $dBase->exec("INSERT INTO users (firstname, lastname, email) VALUES ('$fname','$flastname','$femail')");    
+                        //insert into database if email is valid
+                        if (filter_var($csvData[2], FILTER_VALIDATE_EMAIL)) {
+                            $dBase->exec("INSERT INTO users (name, surname, email) VALUES ('$fname','$flastname','$femail')");  //Execute SQL and insert data into database
+                            $count += 1;        //count the number of records inserted
+                        } else
+                            fwrite(STDOUT, "Error - Invalid email address and unable to write record to database:\nfirstname:".$csvData[0]." lastname:".$csvData[1]." email:".$csvData[2]."\n");    
                     
                     } catch (PDOException $e) {
-                        
-                        echo "Problem inserting userdata: ".$e->getmessage()."\n";
+                        fwrite(STDOUT, "Error inserting userdata into database:\n".$e->getmessage()."\n");      //We had an error on the database inserting this data
                     }
                 }
 
-                if (($GLOBALS['dryrun']) == TRUE)
-                    echo "firstname:".$csvData[0]."  lastname:".$csvData[1]."  email:".$csvData[2]."\n";
+                if ((($GLOBALS['dryrun']) == TRUE) && (filter_var($csvData[2], FILTER_VALIDATE_EMAIL)))
+                    echo "firstname:".$csvData[0]."  lastname:".$csvData[1]."  email:".$csvData[2]."\n";    //We're performing a Dryrun - only display data with valid email
+                elseif ((($GLOBALS['dryrun']) == TRUE) && (!filter_var($csvData[2], FILTER_VALIDATE_EMAIL)))
+                    $errString .= "firstname:".$csvData[0]."  lastname:".$csvData[1]."  email:".$csvData[2]."\n";   //We're performing a Dryrun - data with invalid email added to error string
             }
             $firstline = FALSE;
         }
     }
+    if ((!empty($errString)) && ($errString != NULL))
+        echo "\nThe following data would not be written to the database due to invalid email addresses:\n".$errString;
+
+    if (($GLOBALS['dryrun'] == FALSE) && ($count > 0))
+        echo "\n".$count." records written to database.\n";
 }
 
 function createTable($db) {
     try {
-        
+        //Drop table if Exists. Recreate table & add Unique index constraint.
         $statements = array(
             "DROP TABLE IF EXISTS users;",
-            "CREATE TABLE users (firstname VARCHAR(30), lastname VARCHAR(30), email VARCHAR(50));",
+            "CREATE TABLE users (name VARCHAR(30), surname VARCHAR(30), email VARCHAR(50));",
             "CREATE UNIQUE INDEX email_idx ON users (email);"
         );
 
         foreach ($statements as $statement) {
             $db->exec($statement);
         }
-        //echo "done with table prep...";
+
+        if ($GLOBALS['dryrun'] == FALSE)
+            echo "Database table prep completed...\n";
 
     } catch (PDOException $e) {
         exit ("Problem with database table users: ".$e->getmessage()."\n");
@@ -175,15 +176,12 @@ function createTable($db) {
 
 }
 
-//Create the Database Connection using directive values.  We still use a try\catch incase the RedBean file is missing (RedBean doesn't pass PDO errors back)
+//Create the Database Connection using directive values.  Try\catch to capture connection errors
 function createDBConnection() {  
     try {
-        //$con = mysqli_connect("127.0.0.1:3306","adminer","password","catalyst");  //THIS IS TESTED WORKING with hardcoded
-
-        echo "Trying to create db connection...\n";
         $db = new PDO("mysql:host=".$GLOBALS['host'].";dbname=".$GLOBALS['database'],$GLOBALS['username'],$GLOBALS['password']);
         
-        //echo "connected to database...\n";
+        echo "connected to database...\n";
         return $db;
         
     } catch (PDOException $e) {
@@ -203,8 +201,8 @@ function helpInstructions()
             "-h [host] = set the database host\n".
             "-d [database] = set the database name\n".
             "\n NOTE - If a username, passwword, database or file contains a space, then it needs to be ecapsulated\n".
-            "e.g: `php user_upload.php --file some file.csv -u root -p password -d something -h localhost`  - this will fail\n".
-            "`php user_upload.php -- file \"some file.csv\" -u root -p passwword -d something -h localhost`  - this is correct with quoation marks\n"; 
+            "for example:\n`php user_upload.php --file some file.csv -u root -p password -d something -h localhost`  - this will fail\n".
+            "`php user_upload.php --file \"some file.csv\" -u root -p passwword -d something -h localhost`  - this is correct with quoation marks\n"; 
 
         echo $text."\n";
     }
