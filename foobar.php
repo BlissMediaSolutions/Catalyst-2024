8494644c3680f1b2f5146b86dfdcfc43b309d7d7
BlissMediaSolutions <danielle@bliss.net.au>
2024-03-26 14:06:38 +1100

    cleanup & check foobar

--- foobar.php
+++ foobar.php
@@ -1,20 +1,17 @@
 <?php
     //Part 2 - Logic Test
+    $result = "";
     for ($x=1; $x<=100; $x++) {
         if ($x % 15 == 0 ) {
-            echo "foobar, ";
+            $result = "foobar";
         } elseif ($x % 5 == 0 ) {
-            //not really needed - just didn't like the extra comma on the last number
-            if ($x == 100) {
-                echo "bar";
-            } else {
-                echo "bar, ";
-            }
+            $result = "bar";
         } elseif ($x % 3 == 0 ) {
-            echo "f00, ";
+            $result = "foo";
         } else {
-            echo $x. ", ";
+            $result = $x;
         }
+        echo ($x != 100) ? $result.=", " : $result;
     }
 
     echo "\n";
